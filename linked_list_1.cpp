603d0c0567a75d8937b9caf193ff66ef3979b658
Sumedha <sumedhadogra20@gmail.com>
2023-02-13 16:22:26 +0530

    Initial commit

--- linked_list_1.cpp
+++ linked_list_1.cpp
@@ -51,5 +51,28 @@ int main()
     return 0;
 }
 
-
+/**
+ * @brief head pointer stores the address of the first nide of ll.
+ * null->last node.
+ * class node{
+ *     pointer is pointing next node.=>node* next
+ *     constructor (node) and giving it some value and putting null in next field.   
+ * }
+ * insertattail{
+ *     node*&head - by reference
+ *     node*n=new node(val) => a node whose data field contains val and next contains null.
+ *     to traverse the linked list we need a iterator, so we use a ponter node*temp, when temp->next is null we will 
+ *     know that it's the last element. and when we reach that point we add n to the next of the temp.
+ *     
+ *     if head is null,then head=n, head becomes first node.
+ * }
+ * 
+ * display{
+ *     to display the linked list.
+ *     we need to make temp pointer which starts with head. and traverse till temp is not equal to null
+ *     and print.
+ * }
+ * 
+ * 
+ */
  
